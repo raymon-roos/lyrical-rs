e7abe25371afed4c2e6c684b4220cfd62dbdb0b4
Raymon Roos <raymon.roos@hotmail.com>
2025-06-02 13:55:18 +0200

    refactor: clippy suggestion & fully qualify token error variants

--- src/token.rs
+++ src/token.rs
@@ -1,24 +1,22 @@
-use crate::token::Error::{FileEmpty, FileUnreadable};
-use std::fs::read_to_string;
-
 /// Read the Genius API token from `$XDG_CONFIG_HOME/lyrical/token`.
 ///
 /// If `$XDG_CONFIG_HOME` is not set, the fallback value is `$HOME/.config`.
 pub fn read_from_file() -> Result<String, Error> {
     let token_path = config_directory() + "/lyrical/token";
 
-    let token = read_to_string(&token_path).map_err(|_| FileUnreadable(token_path.clone()))?;
+    let token = std::fs::read_to_string(&token_path)
+        .map_err(|_| Error::FileUnreadable(token_path.clone()))?;
 
     Ok(token
         .lines()
         .next()
-        .ok_or(FileEmpty(token_path))?
+        .ok_or(Error::FileEmpty(token_path))?
         .to_string())
 }
 
 fn config_directory() -> String {
     match std::env::var("XDG_CONFIG_HOME") {
-        Ok(xdg_config_home) => String::from(xdg_config_home),
+        Ok(xdg_config_home) => xdg_config_home,
         Err(_) => format!("{}/.config", std::env::var("HOME").unwrap_or_default()),
     }
 }
